487c0cb991a4af107c374bbb6d720261c0214d0a
Jakob Bethmann <jvb@MacBook-Pro-109.local>
2020-05-05 09:45:48 +0100

    adding new helper methods

---
+++ src/Helpers/filterMatrix.js
@@ -0,0 +1,20 @@
+export const filterMatrix = {
+  'All ToDos': {
+    'I can see': 0,
+    'for me': 1,
+    'for me & everyone': 2,
+    'appointed by me': 3
+  },
+  'Open ToDos': {
+    'I can see': 4,
+    'for me': 5,
+    'for me & everyone': 6,
+    'appointed by me': 7
+  },
+  'Done ToDos': {
+    'I can see': 8,
+    'for me': 9,
+    'for me & everyone': 10,
+    'appointed by me': 11
+  }
+}
---
+++ src/Helpers/valueFormatter.js
@@ -0,0 +1,33 @@
+export const valueFormatter = (value, format) => {
+  let currencyFormatter = new Intl.NumberFormat('de-DE', {
+    style: 'currency',
+    currency: 'EUR',
+  });
+  let formattedValue = '';
+
+  switch (format) {
+    case 'Age':
+      formattedValue = `${value}`
+      break;
+    case '% str':
+      formattedValue = `${(value * 100).toFixed(2).replace('.', ',')} %`;
+      break;
+    case '%':
+      formattedValue = `${value} %`;
+      break;
+    case '% p.a.':
+      formattedValue = `${value / 10}% p.a.`;
+      break;
+    case 'EUR':
+      formattedValue = currencyFormatter.format(value);
+      break;
+    case 'inYears':
+      formattedValue = `in ${value} Jahren`;
+      break;
+    case 'years':
+      formattedValue = `${value} Jahre`;
+      break;
+  }
+
+  return formattedValue;
+}
