f2be525160325624ceac78b2621fb12c7911e6e9
JakobBe <jakob@bethmann.de>
2020-05-18 11:49:53 +0100

    updating extras

--- src/Helpers/magicNumbers.js
+++ src/Helpers/magicNumbers.js
@@ -1,2 +1,7 @@
 export const defaultId = '00000000-0000-0000-0000-000000000000';
 export const dateTimeFormat = 'YYYY-MM-DDTHH:mm:ssZ';
+export const invitaionStatus = {
+  pending: '0',
+  accepted: '1',
+  declined: '2'
+};
